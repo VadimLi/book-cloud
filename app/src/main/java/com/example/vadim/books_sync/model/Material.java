2b2f37af395343256ec7d7bc656b1e4278f122b1
Vadim Lipakov <revalier01@mail.ru>
2018-06-22 15:05:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vadim/books_sync/model/Material.java
@@ -0,0 +1,14 @@
+package com.example.vadim.books_sync.model;
+
+public class Material {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
